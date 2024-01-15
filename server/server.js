ea37ef1d6189ea9507150a58659280c423061dfc
cethread <threadgillcheyenne@gmail.com>
2024-01-15 18:27:46 -0500

    revisions

--- server/server.js
+++ server/server.js
@@ -9,6 +9,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 const PORT = 8080;
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, DELETE");
+  next();
+});
+
 // DATABASE CONNECTION & AUTH
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
