56d4f000bdfab6630c39ea50724e1e91a248d6ea
cethread <threadgillcheyenne@gmail.com>
2024-01-15 18:44:14 -0500

    revisions

--- server/server.js
+++ server/server.js
@@ -6,16 +6,11 @@ const cors = require("cors");
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: ["https://kropp-gym.netlify.app"],
-    methods: ["POST", "GET", "DELETE"],
-  })
-);
+app.use(cors());
 const PORT = 8080;
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://kropp-gym.netlify.app");
   res.header("Access-Control-Allow-Headers", " Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, DELETE");
   next();
