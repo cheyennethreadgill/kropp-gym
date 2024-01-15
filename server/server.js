2b7dba66de309857ed3808dbd2b1a3d5425bdc8b
cethread <threadgillcheyenne@gmail.com>
2024-01-15 18:33:12 -0500

    revisions

--- server/server.js
+++ server/server.js
@@ -14,6 +14,16 @@ app.use(
 );
 const PORT = 8080;
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, DELETE");
+  next();
+});
+
 // DATABASE CONNECTION & AUTH
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
