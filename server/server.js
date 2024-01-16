5f78e95f603f1b9f48a41153a5998e4177b359ff
cethread <threadgillcheyenne@gmail.com>
2024-01-16 07:50:20 -0500

    cors

--- server/server.js
+++ server/server.js
@@ -7,16 +7,16 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // use to enable all cors requests
-// app.use(cors());
+app.use(cors());
 const PORT = 8080;
 
 // same as cors config below
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", " Content-Type, Accept");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Headers", " Content-Type, Accept");
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE");
+//   next();
+// });
 
 // DATABASE CONNECTION & AUTH
 const db = mysql.createConnection({
