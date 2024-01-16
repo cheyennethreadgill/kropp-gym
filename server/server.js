3a30cf21117aaf25d041399659b27d5ad057f1b7
cethread <threadgillcheyenne@gmail.com>
2024-01-16 11:31:36 -0500

    cors

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // use to enable all cors requests
-app.use(cors());
+// app.use(cors());
 const PORT = 8080;
 
 // same as cors config below
@@ -79,22 +79,21 @@ app.get("/newsletter", (req, res) => {
 
 // after
 // use to enable cors for a single route
-// var corsOptions = {
-//   origin: "https://kropp-gym.netlify.app",
-//   // origin: "https://kropp-gym.netlify.app/Checkout",
-//   methods: ["GET", "POST", "DELETE"],
-//   allowedHeaders: ["Content-Type", "Accept"],
-//   exposedHeaders: ["Origin, Methods", "allowedHeaders"],
-//   preflightContinue: false,
-//   optionsSuccessStatus: 204,
-// };
+var corsOptions = {
+  origin: "kropp-gym.netlify.app",
+  // origin: "https://kropp-gym.netlify.app/Checkout",
+  methods: ["GET", "POST", "DELETE"],
+  allowedHeaders: ["Content-Type", "Accept"],
+  exposedHeaders: ["Origin, Methods", "allowedHeaders"],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
 // enable preflight cors options for client preflight request
-// app.options("/customerorder", cors(corsOptions));
+app.options("/customerorder", cors(corsOptions));
 
 // INSERT INTO CUSTOMER ORDER TABLE
 //    handles single rote with cors middleware
-app.post("/customerorder", (req, res) => {
-  // app.post("/customerorder", cors(corsOptions), (req, res) => {
+app.post("/customerorder", cors(corsOptions), (req, res) => {
   // app.post("/customerorder", (req, res) => {
   let customerID = req.body.customerID;
   let FName = req.body.FName;
