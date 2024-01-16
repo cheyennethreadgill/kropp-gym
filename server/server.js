efcd49f9580b1a5fbb45abdfe853f85a517e4aa5
cethread <threadgillcheyenne@gmail.com>
2024-01-16 10:31:29 -0500

    cors

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const PORT = 8080;
 
 // same as cors config below
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE");
   next();
