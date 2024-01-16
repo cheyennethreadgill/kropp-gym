1ee1ea0c97066839b1b6680377a5a0272c81a6a8
cethread <threadgillcheyenne@gmail.com>
2024-01-16 07:48:51 -0500

    cors

--- client/src/Components/Checkout/Checkout.jsx
+++ client/src/Components/Checkout/Checkout.jsx
@@ -101,7 +101,7 @@ const Checkout = ({ date, cartLength, cart, grandTotal }) => {
           Accept: "application/json, text/plain",
           "Content-Type": "application/json",
         },
-        mode: "cors",
+        mode: "no-cors",
         body: JSON.stringify({
           FName: FName,
           LName: LName,
