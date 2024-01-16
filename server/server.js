63b170c222f78a48cc6a7e5b90094cd2dd6d9ab3
cethread <threadgillcheyenne@gmail.com>
2024-01-16 10:02:39 -0500

    cors

--- server/server.js
+++ server/server.js
@@ -79,16 +79,16 @@ app.get("/newsletter", (req, res) => {
 
 // after
 // use to enable cors for a single route
-// var corsOptions = {
-//   origin: true,
-//   methods: ["GET", "POST", "DELETE"],
-//   allowedHeaders: ["Content-Type", "Accept"],
-//   exposedHeaders: ["Origin, Methods", "allowedHeaders"],
-//   preflightContinue: false,
-//   optionsSuccessStatus: 204,
-// };
+var corsOptions = {
+  origin: true,
+  methods: ["GET", "POST", "DELETE"],
+  allowedHeaders: ["Content-Type", "Accept"],
+  exposedHeaders: ["Origin, Methods", "allowedHeaders"],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
 // enable preflight cors options for client preflight request
-// app.options("/customerorder", cors(corsOptions));
+app.options("/customerorder", cors(corsOptions));
 
 // INSERT INTO CUSTOMER ORDER TABLE
 //    handles single rote with cors middleware
