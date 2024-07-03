a57f48ce7a958515063951c8112725bc682dba50
cethread <threadgillcheyenne@gmail.com>
2024-07-03 18:07:41 -0400

    UI revisions: fixed main carousel on homepage

--- client/src/Components/Home/Header.jsx
+++ client/src/Components/Home/Header.jsx
@@ -54,7 +54,7 @@ const Header = ({ carouselImgMode, darkMode, cartLength, cart }) => {
           return (
             <Carousel.Item
               key={img.id}
-              interval={4500}
+              interval={4100}
             >
               <Image
                 fluid
